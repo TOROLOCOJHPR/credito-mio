a9b90daa0915a80e49e5c7337f691fc045629951
TOROLOCOJHPR <jesusparra07hp@gmail.com>
2018-04-28 07:06:38 -0600

    crear estructura base

---
+++ 404.php
@@ -0,0 +1,1 @@
+error página 404
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php $titulo = "credito-mio"?>
+<!DOCTYPE html>
+<html lang="es">
+<?php include "include/header.php"?>
+<body>
+<?php include "mod/header.php"?>
+<?php include "mod/footer.php"; include "include/jsFiles.php"?>    
+</body>
+</html>
